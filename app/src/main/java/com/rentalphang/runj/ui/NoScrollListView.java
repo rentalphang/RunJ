0934c3aeca2d50067cca010936db7e3130254849
jay529764604 <pengwentao@qq.com>
2017-01-02 00:39:53 +0800

    beta1

--- app/src/main/java/com/rentalphang/runj/ui/NoScrollListView.java
+++ app/src/main/java/com/rentalphang/runj/ui/NoScrollListView.java
@@ -9,7 +9,6 @@ import android.widget.ListView;
  *
  * scrollview嵌套listview,解决滑动冲突问题
  *
- * Created by 洋 on 2016/5/16.
  */
 public class NoScrollListView extends ListView {
     public NoScrollListView(Context context) {
